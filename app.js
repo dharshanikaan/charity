443fef13e1e418c00048736bb5fe88b418eb0c57
negha <neghashree96@gmail.com>
2024-11-25 23:30:14 +0530

    second part

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-rtiuodfjkfl;xcv,bfhdgeuiosdflcvx
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./utils/database");
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+// routes
+const user = require("./routes/user");
+const donation = require("./routes/donation");
+const charity = require("./routes/charity");
+const project = require("./routes/project");
+const download = require("./routes/download");
+const report = require("./routes/impactReport");
+// models
+
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+  method: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+// middleware
+const app = express();
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use("/api/users", user);
+app.use("/api/donations", donation);
+app.use("/api/charities", charity);
+app.use("/api/projects", project);
+app.use("/api/", download);
+app.use("/api/reports", report);
+
+async function testConnection() {
+  try {
+    await db.authenticate();
+    console.log("Connection has been established successfully.");
+    await db.sync();
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+testConnection();
+
+app.listen(port, () => {
+  console.log("Server started on port 3000");
+});
